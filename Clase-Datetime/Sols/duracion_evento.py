34e27b763d0770370f9ed190de36199e377eb3d7
Patricklt2 <ptorlaschis@gmail.com>
2024-03-19 17:01:11 -0300

    ejemplos

--- Clase-Datetime/Sols/duracion_evento.py
+++ Clase-Datetime/Sols/duracion_evento.py
@@ -6,3 +6,16 @@ hora de finalización de un evento, y calcule la duración total del evento en h
 
 import datetime
 
+
+def dur_evento():
+    try:
+        inicio = datetime.datetime.fromisoformat(input("Ingrese fecha y hora en formato YYYY-MM-DDThh:mm:ss"))
+        fin = datetime.datetime.fromisoformat(input("Ingrese fecha de fin en formato YYYY-MM-DDThh:mm:ss"))
+
+        aux = fin - inicio
+        print("Horas: ", aux.days*24, "Minutos: ")
+    except IOError:
+        print("Error de input")
+
+
+dur_evento()

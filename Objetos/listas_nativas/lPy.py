e598971a8a771fa5e86e160e44b03ce9795e06bf
Patricklt2 <ptorlaschis@gmail.com>
2024-03-28 18:20:36 -0300

    ejemplos

---
+++ Objetos/listas_nativas/lPy.py
@@ -0,0 +1,16 @@
+"""
+Las listas_nativas en python son agrupaciones de objetos (o mas bien listas_nativas de objetos).
+Las listas_nativas son modificables y transformables.
+"""
+
+# ejemplos de listas_nativas
+
+lista = [1, 2, 3, 4, 5]
+
+# lista de listas_nativas
+
+lista2 = [[1, 2, 3], [1, 2, 3]]
+
+# lista de strings lo que es equivalente a una lista de listas_nativas de caracteres o cadenas
+
+lista3 = ["aaaa", "bbbb", "cccc"]
